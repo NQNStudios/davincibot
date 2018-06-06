8d7cf444d97dde3f6472ebe80730cc8e9b7d8b42
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-06-06 14:18:53 -0400

    Idea progress tests are passing

--- src/Idea.ts
+++ src/Idea.ts
@@ -1,27 +1,29 @@
 // The basic unit of things the user wants to do. Ideas are all created in
 // unstructured form, and structured later by dividing and subdividing them
 // into smaller Ideas.
-class Idea
+export class Idea
 {
     // Each Idea will have a unique id numbered upwards from 0.
-    static Count: int = 0;
+    // TODO this will need to be serialized/deserialized when Tasks are reloaded from
+    // a previous session!
+    static Count: number = 0;
 
-    id: int;
+    id: number;
     name: string;
     description: string = '';
     tags: Array<string> = [];
 
     children: Array<Idea> = [];
-    private _progress: float = 0;
+    private _progress: number = 0;
 
     // Create an unstructured Idea
     constructor(name: string) {
-        this.id = Count++;
+        this.id = Idea.Count++;
         this.name = name;
     }
 
     // Check how near this Idea is to tangible completion
-    get progress(): boolean {
+    get progress(): number {
         // Only Ideas without children can define their own progress
         if (this.children.length == 0) {
             return this._progress;
@@ -30,7 +32,8 @@ class Idea
         // its children's progress values
         else {
             let sum = 0;
-            for (var child: Idea in this.children) {
+            for (let i = 0; i < this.children.length; ++i) {
+                let child = this.children[i];
                 sum += child.progress;
             }
             return sum / this.children.length;
@@ -39,7 +42,23 @@ class Idea
 
     // Set how near this Idea is to tangible completion --
     // Only valid for an undivided Idea
-    set progress(value): boolean {
-        this._progress = value;
+    set progress(value: number) {
+        if (this.children.length == 0) {
+            if (value >= 0 && value <= 1) {
+                this._progress = value;
+            }
+            else {
+                throw new Error(`Tried to set an Idea's progress out of bounds [0:1]: ${value}`);
+            }
+        }
+        else {
+            throw new Error('Tried to directly set the progress of a task with children.');
+        }
+    }
+
+    addChild(name: string) {
+        let newChild = new Idea(name); 
+        this.children.push(newChild);
+        return newChild;
     }
 }
