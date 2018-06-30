c79e49f67e34882dcf5bbaa66fab1875792a3b5b
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-06-29 22:39:06 -0400

    Renaming Ideas.ts

--- src/Idea.ts
+++ src/Idea.ts
@@ -1,16 +1,32 @@
 import * as db from 'quick.db';
 
-// The basic unit of user data in Da Vinci Bot.
-// TODO write clear documentation on this
-export class Idea = {
-    readonly id: number;
+type IdeaData = {
+    id: number;
     name: string = '';
     description: string = '';
     tags: Array<string> = [];
-    children: Array<number> = [];
+    childIds: Array<number> = [];
+}
+
+// The basic unit of user data in Da Vinci Bot.
+// TODO write clear documentation on the Idea metaphor/design pattern
+export class Idea {
+    readonly data: IdeaData;
+
+    // TODO add properties for direct access to data fields?
+
+    // TODO allow pre-initialization of child Ideas as objects with a recursion
+    // depth specifier
 
     private constructor(id: number) {
         this.id = id;
+    }
+
+    static async Initialize(): void {
+        let ideaCount = await db.fetch('IdeaCount');
+        if (ideaCount === null) {
+            db.set('IdeaCount', 0);
+        }
     }
 
     static async Get(id: number) {
@@ -20,14 +36,6 @@ export class Idea = {
 
 export class Ideas
 {
-    static async Initialize(): void {
-        let ideaCount = await db.fetch('IdeaCount');
-        if (ideaCount === null) {
-            db.set('IdeaCount', 0);
-            let rootIdea = NewIdea();
-        }
-    }
-
     static async NewIdea(): Idea {
         let ideaCount = await db.fetch('IdeaCount');
         db.add('IdeaCount', 1);
