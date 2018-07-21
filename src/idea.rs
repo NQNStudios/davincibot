0953515d9d9a69958c374795517ee127db99ae5a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-07-20 21:37:00 -0400

    Idea accessors all compile

--- src/idea.rs
+++ src/idea.rs
@@ -3,7 +3,8 @@ extern crate serde_json;
 
 use std::path::Path;
 
-use rusqlite::{Connection};
+use rusqlite::{Connection, RowIndex};
+use rusqlite::types::{Value};
 
 use error::*;
 
@@ -13,13 +14,13 @@ use error::*;
 // TODO explain exactly how Ideas work and why
 #[derive(Debug, Default)]
 pub struct Idea {
-    id: usize,
+    id: i64,
     name: String,
     description: String,
     tags: Vec<String>,
 
-    parent_id: Option<usize>,
-    child_ids: Vec<usize>,
+    parent_id: Option<i64>,
+    child_ids: Vec<i64>,
     // TODO add attachments to Ideas
     // TODO add extra serde-serializable data to Ideas
 }
@@ -65,12 +66,44 @@ impl IdeaTree {
     }
 
     pub fn get_tags(&self, id: i64) -> Result<Vec<String>> {
-        let tags_json: String = self.conn.query_row("SELECT description FROM ideas where id=?", &[&id], |row| { row.get(3) })?;
+        let tags_json: String = self.conn.query_row("SELECT description FROM ideas WHERE id=?", &[&id], |row| { row.get(3) })?;
         let tags: Vec<String> = serde_json::from_str(tags_json.as_str())?;
         Ok(tags)
     }
 
-    pub fn get_idea(&self, id: u64) -> Result<Idea> { 
-        Ok(Idea { .. Default::default() })
+    pub fn get_parent_id(&self, id: i64) -> Result<Option<i64>> {
+        let parent_id: Value = self.conn.query_row("SELECT parent_id FROM ideas WHERE id=?", &[&id], |row| { row.get(4) })?;
+        match parent_id {
+            Value::Null => Ok(None),
+            Value::Integer(id) => Ok(Some(id)),
+            _ => panic!("The parent_id row of an Idea in the database is neither Null nor an ID!")
+        }
+    }
+
+    pub fn get_child_ids(&self, id: i64) -> Result<Vec<i64>> {
+        let child_ids_json: String = self.conn.query_row("SELECT child_ids FROM ideas WHERE id=?", &[&id], |row| { row.get(5) })?;
+        let child_ids: Vec<i64> = serde_json::from_str(child_ids_json.as_str())?;
+        Ok(child_ids)
+    }
+
+    pub fn get_idea(&self, id: i64) -> Result<Idea> { 
+        let idea: Result<Idea> = self.conn.query_row("SELECT * FROM ideas WHERE id=?", &[&id], |row| -> Result<Idea> {
+            let tags = serde_json::from_str(row.get::<i32, String>(3).as_str())?;
+            let child_ids = serde_json::from_str(row.get::<i32, String>(5).as_str())?;
+
+            Ok(Idea {
+                id,
+                name: row.get(1),
+                description: row.get(2),
+                tags,
+                parent_id: match row.get(4) {
+                    Value::Null => None,
+                    Value::Integer(id) => Some(id),
+                    _ => panic!("The parent_id row of an Idea in the database is neither Null nor an ID!")
+                },
+                child_ids,
+            })
+        })?;
+        idea
     }
 }
