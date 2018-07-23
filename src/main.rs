d74bbb1e48cbbd84a691e08d069035293a7b08fa
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-07-22 23:15:59 -0400

    Allowing file specification

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::path::PathBuf;
+
 extern crate rusqlite;
 extern crate serde_json;
 
@@ -7,7 +10,14 @@ mod idea;
 use idea::*;
 
 fn main() {
-    let tree = IdeaTree::open("test.sqlite").expect("Failed to create Da Vinci tree."); 
+    let home_path = env::home_dir().unwrap_or(PathBuf::new());
+    let home_path = home_path.to_str().unwrap();
+    let filename = env::args().skip(1).next().unwrap_or(format!("{}/project.dv", home_path));
+
+    println!("Loading Da Vinci file: {}", filename);
+    let mut tree = IdeaTree::open(filename).expect("Failed to create Da Vinci tree."); 
+
+
 }
 
 // TODO Interrupt ^C signal and treat it as "exit" instead of closing program
