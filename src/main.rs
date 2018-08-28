b22308480fa16dc47017f2db2617eed8501b24e3
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-08-28 15:04:42 -0400

    Added a --version output

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,26 @@ extern crate davincibot;
 use davincibot::idea::IdeaTree;
 use davincibot::repl::Repl;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+
 fn main() {
     let home_path = env::home_dir().unwrap_or(PathBuf::new());
     let home_path = home_path.to_str().unwrap();
-    let filename = env::args().skip(1).next().unwrap_or(format!("{}/project.dv", home_path));
 
-    println!("Loading Da Vinci file: {}", filename);
-    let mut tree = IdeaTree::open(filename).expect("Failed to create Da Vinci tree."); 
+    let VERSION_COMMANDS: Vec<&str> = vec!["-v", "-version", "--v", "--version", ];
 
-    Repl::new().run(&mut tree);
+    let arg = env::args().skip(1).next().unwrap_or(format!("{}/project.dv", home_path));
+
+    if arg.chars().next() == Some('-') && VERSION_COMMANDS.contains(&arg.as_str()) {
+        println!("Da Vinci Bot version: {}", VERSION);
+    } else {
+
+        println!("Loading Da Vinci file: {}", arg);
+        let mut tree = IdeaTree::open(arg).expect("Failed to create Da Vinci tree."); 
+
+        Repl::new().run(&mut tree);
+    }
 }
 
 // TODO Interrupt ^C signal and treat it as "exit" instead of closing program
